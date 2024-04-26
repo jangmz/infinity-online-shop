82f6589a1b841f7ec0db4587e5b5cfd6c698349d
Jan <131482887+jangmz@users.noreply.github.com>
2024-04-26 11:01:01 +0200

    created errorPage, shop page, created routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  
+  return (
+    <h1>Hello</h1>
+  )
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Shop from "./components/shop.jsx"
+import ErrorPage from "./components/error-page.jsx"
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path:"shop",
+    element: <Shop />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
