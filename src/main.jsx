be04740590c4e665d4acbca7eb63b830a2944435
Jan <131482887+jangmz@users.noreply.github.com>
2024-04-26 11:13:20 +0200

    removed default styling

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Shop from "./components/shop.jsx"
 import ErrorPage from "./components/error-page.jsx"
-import './index.css'
+//import './index.css'
 import Cart from './components/cart.jsx'
 
 const router = createBrowserRouter([
