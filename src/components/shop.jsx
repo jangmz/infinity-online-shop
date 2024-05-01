8b134a1034acda9fc37ae9b96d54a06eafc2e5a3
Jan <131482887+jangmz@users.noreply.github.com>
2024-05-01 20:26:46 +0200

    working on adding to cart, continue at shop->onAddToCart

--- src/components/shop.jsx
+++ src/components/shop.jsx
@@ -1,18 +1,27 @@
 import { useState, useEffect, useContext } from "react"
 import ProductCard from "./product-card"
 import ErrorPage from "./error-page"
+import { CartContext } from "../root"
 
 const Shop = () => {
     const [products, setProducts] = useState([])
     const [error, setError] = useState(null)
     const [loading, setLoading] = useState(true)
+    const { cart, setCart } = useContext(CartContext)
 
     // add sorting state and function that call the API -> has sorting feature
 
     // how will it be updated? receives a product? receives a new array that replaces old one?
     // what happens if items are removed not added? how to udpate that?
+
+    // function adds product to the cart -> finds item via "productID"
     const onAddToCart = (productID, quantity) => {
         console.log("Add to cart products: "+ quantity +", ID: " + productID)
+        const newCart = [...cart]
+        const ProductToAdd = products.filter(product => product.id === productID)
+        newCart.push(ProductToAdd[0])
+        console.log("Cart:")
+        console.log(newCart)
     }
 
     useEffect(() => {
