bf45dfc331b49f27eba0f3d59f2d36e82dde431c
Jan <131482887+jangmz@users.noreply.github.com>
2024-05-02 10:56:54 +0200

    removed unnecessary code

--- src/root.jsx
+++ src/root.jsx
@@ -8,30 +8,8 @@ export const CartContext = createContext()
 
 function Root() {
   const location = useLocation()
-  const [cart, setCart] = useState([
-    /*{
-      id: 1,
-      title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-      price: 109.95,
-      quantity: 1,
-      description: "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-      category: "men's clothing",
-      image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-      rating: {
-          rate: 3.9,
-          count: 120
-      }
-    }*/
-  ])
+  const [cart, setCart] = useState([])
   const numProducts = cart.length
-
-  const incrementNumProducts = () => {
-    //setNumProducts((prevNumProducts) => prevNumProducts + 1)
-  }
-
-  const decrementNumProducts = () => {
-    //setNumProducts((prevNumProducts) => prevNumProducts - 1)
-  }
   
   // check if the current route is not the root route
   const isSubRoute = location.pathname !== "/"
